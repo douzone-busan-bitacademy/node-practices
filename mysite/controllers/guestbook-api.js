03a68f5f8d4ae74940784f7671977cd6f5b39cc1
douzone-busan-bitacademy <83158968+douzone-busan-bitacademy@users.noreply.github.com>
2021-07-16 16:56:58 +0900

    Update guestbook-api.js

--- mysite/controllers/guestbook-api.js
+++ mysite/controllers/guestbook-api.js
@@ -1,49 +1,43 @@
+const models = require('../models');
+const { Op } = require("sequelize");
+
 module.exports = {
-    create: async function(req, res, next) {
-        console.log(req.body);
-        // sql insert
-        res.status(200).send({
+    create: async function(req, res, next) {   
+        const result = await models.Guestbook.create(req.body);
+        res.send({
             result: 'success',
-            data: Object.assign(req.body, {
-                no: 10,
-                password: '',
-                regDate: new Date()
-            }),
+            data: result,
             message: null
         });
     },
     read: async function(req, res, next) {
         const startNo = req.query.sno || 0;
-        console.log(startNo);
-        // sql: select.... limit
-        res.status(200).send({
+        const results = await models.Guestbook.findAll({
+            attributes: ['no', 'name', 'message'],
+            where: (startNo > 0) ? {no: {[Op.lt]: startNo}} : {},
+            order: [
+                ['no', 'desc']
+            ],
+            limit: 3
+        });
+
+        res.send({
             result: 'success',
-            data:[{
-                no: 9,
-                name: '둘리',
-                message: '호이~',
-                regDate: new Date()
-            }, {
-                no: 8,
-                name: '마이콜',
-                message: '구공탄~',
-                regDate: new Date()
-            }, {
-                no: 7,
-                name: '도우너',
-                message: '둘리야~',
-                regDate: new Date()
-            }],
+            data: results,
             message: null
         });
     },
-    delete: function(req, res, next) {
-        console.log(req.params.no + ":" + req.body.password);
-        // sql delete
-        res.status(200).send({
-            result: 'success',
-            data: req.params.no,
-            message: null
+    delete: async function(req, res, next) {
+        const no = req.params.no || 0;
+        const result = await models.Guestbook.destroy({
+            where: {
+                no: no
+            }
         });
+        res.send({
+            result: 'success',
+            data: no,
+            message: null
+        });       
     }
 }
