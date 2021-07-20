deba372004a53b3c2e307206f080a1c228be553f
douzone-busan-bitacademy <douzone.busan.bitacademy@gmail.com>
2021-07-20 11:25:55 +0900

    Association Mapping #2

--- mysite/test/Board.test.js
+++ mysite/test/Board.test.js
@@ -46,6 +46,20 @@ describe('Model Board', function(){
         board.no.should.not.equal(undefined);
     });
 
+    it("Fetch Boards by user(test)", async function(){
+        const results = await models.Board.findAll({
+            where: {
+                userNo: user.no
+            },
+            include: {
+                model: models.User,
+                required: true
+            }
+        });
+
+        results.should.have.lengthOf(3);
+    });
+
     after(async function(){
         await models.Board.destroy({
             where: {
